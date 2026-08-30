5c1a1d4be9cbcd8490ea145a99a5e8db0bf1e4f5
feedbackwebnet <feedbackwebnet@users.noreply.github.com>
2026-08-30 00:18:53 +0000

    deploy: 845ffd7510e7556807847c1c5756b621e1aa4e50 feedbackwebnet/lumoria@845ffd7510e7556807847c1c5756b621e1aa4e50

---
+++ _nuxt/HQ11OP6f2.js
@@ -0,0 +1,1 @@
+import{a as e,n as t}from"./CDGZPHQ1.js";import{l as n}from"./nQgdnVHw.js";var r=`reeland_settings`,i={preferredPlayer:void 0,commentsEnabled:!0},a=t(`settings`,()=>{let t=e(n(r,{...i},{deep:!0,initOnMounted:!0,mergeDefaults:!0,onError:e=>{}}));function a(e,n){t.value[e]=n}return{settings:t,update:a}});export{a as t};
